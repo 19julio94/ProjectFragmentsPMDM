6b33c11a64b3e6fce96f3d82a9293f6b7522d222
19julio94 <juliopatriciodasilva@gmail.com>
2016-12-15 23:55:21 +0100

    Parte donde un Fragment llama a una segunda activity a traves de un boton,hemos creado el boton.

--- app/src/main/java/com/example/jp/projectfragmentspmdm/ActivityPrincipalFragment.java
+++ app/src/main/java/com/example/jp/projectfragmentspmdm/ActivityPrincipalFragment.java
@@ -1,10 +1,13 @@
 package com.example.jp.projectfragmentspmdm;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,10 +16,29 @@ public class ActivityPrincipalFragment extends Fragment {
 
     public ActivityPrincipalFragment() {
     }
-
+    private Button boton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_activity_principal, container, false);
+
+        View view=inflater.inflate(R.layout.fragment_activity_principal, container, false);
+        boton = (Button)view.findViewById(R.id.bplay);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(),ActivitySecond.class);
+
+                startActivity(intent);
+
+                Toast.makeText(getActivity(), "Esto es la segunda Activity", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+        return view;
     }
 }
