9f369c3633584d947a91ca56b12299e56246e0cc
19julio94 <juliopatriciodasilva@gmail.com>
2016-12-16 00:50:27 +0100

    Parte dos casi hecha

--- app/src/main/java/com/example/jp/projectfragmentspmdm/ActivityPrincipalFragment.java
+++ app/src/main/java/com/example/jp/projectfragmentspmdm/ActivityPrincipalFragment.java
@@ -1,5 +1,6 @@
 package com.example.jp.projectfragmentspmdm;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -13,9 +14,22 @@ import android.widget.Toast;
  * A placeholder fragment containing a simple view.
  */
 public class ActivityPrincipalFragment extends Fragment {
-
-    public ActivityPrincipalFragment() {
+    Comunicador cm;
+    public interface Comunicador{
+        public void mensaje(String texto);
     }
+
+    public void onAttach(Context contexto){
+        super.onAttach(contexto);
+        cm = (Comunicador)contexto;
+    }
+    final static String txt = "He vuelto";
+    public ActivityPrincipalFragment() {
+
+
+    }
+
+
     private Button boton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,19 +43,20 @@ public class ActivityPrincipalFragment extends Fragment {
             public void onClick(View v) {
 
                 if(getResources().getBoolean(R.bool.modoDosPantallas)){
-                    Toast toast = Toast.makeText(getActivity(),"Estoy en Landscape", Toast.LENGTH_SHORT);
-                    toast.show();
-
+                    //Toast toast = Toast.makeText(getActivity(),"Estoy en Landscape", Toast.LENGTH_SHORT);
+                    //toast.show();
+                    cm.mensaje("Te tengo que decir adios");
 
                 }else {
 
-                    Toast toast = Toast.makeText(getActivity(), "Estoy en Portrait", Toast.LENGTH_SHORT);
-                    toast.show();
-
-
-                    //Intent intent = new Intent(getActivity(), ActivitySecond.class);
-
+                   // Toast toast = Toast.makeText(getActivity(), "Estoy en Portrait", Toast.LENGTH_SHORT);
+                   // toast.show();
+                     //Intent intent = new Intent(getActivity(), ActivitySecond.class);
                     //startActivity(intent);
+
+                    Intent i= new Intent(getActivity(),ActivitySecond);
+                    i.putExtra(ActivityPrincipalFragment.txt,"Ah no,Me he ido jajajaja");
+                    startActivity(i);
 
 
                 }
