57cc7b40c7bd283fe38f5867855995f44602ebef
19julio94 <juliopatriciodasilva@gmail.com>
2016-12-15 23:32:36 +0100

    Commmit inicial

---
+++ app/src/main/java/com/example/jp/projectfragmentspmdm/ActivityPrincipalFragment.java
@@ -0,0 +1,22 @@
+package com.example.jp.projectfragmentspmdm;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ActivityPrincipalFragment extends Fragment {
+
+    public ActivityPrincipalFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_activity_principal, container, false);
+    }
+}
